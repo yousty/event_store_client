ab2a767dc505a0dcb1afd30eba79ab2d00f69e85
Konrad Badzioch <konrad.badzioch@useo.pl>
2019-12-02 12:09:48 +0100

    Add tests for Api Client

--- spec/event_store_client/store_adapter/api/client_spec.rb
+++ spec/event_store_client/store_adapter/api/client_spec.rb
@@ -10,7 +10,7 @@ module EventStoreClient::StoreAdapter::Api
     describe '#append_to_stream' do
       let!(:request) { stub_request(:post, 'https://www.example.com:8080/streams/stream_name') }
 
-      it 'sends request' do
+      it 'sends a correct request' do
         events = [
           EventStoreClient::Event.new(type: 'SomethingHappened', data: { foo: 'bar' }.to_json),
           EventStoreClient::Event.new(type: 'SomethingElseHappened', data: { foo: 'bar' }.to_json)
@@ -23,19 +23,52 @@ module EventStoreClient::StoreAdapter::Api
     describe '#delete_stream' do
       let!(:request) { stub_request(:delete, 'https://www.example.com:8080/streams/stream_name') }
 
-      it 'sends request' do
+      it 'sends a correct request' do
         api_client.delete_stream('stream_name', true)
         expect(request).to have_been_requested
       end
     end
 
     describe '#read' do
+      let!(:request) do
+        stub_request(
+          :get,
+          'https://www.example.com:8080/streams/stream_name/0/forward/20?embed=body'
+        )
+      end
+
+      it 'sends a correct request' do
+        api_client.read('stream_name', count: 20)
+        expect(request).to have_been_requested
+      end
     end
 
     describe '#subscribe_to_stream' do
+      let!(:request) do
+        stub_request(
+          :put,
+          'https://www.example.com:8080/subscriptions/stream_name/subscription_name'
+        )
+      end
+
+      it 'sends a correct request' do
+        api_client.subscribe_to_stream('stream_name', 'subscription_name')
+        expect(request).to have_been_requested
+      end
     end
 
     describe '#consume_feed' do
+      let!(:request) do
+        stub_request(
+          :get,
+          'https://www.example.com:8080/subscriptions/stream_name/subscription_name/10?embed=body'
+        )
+      end
+
+      it 'sends a correct request' do
+        api_client.consume_feed('stream_name', 'subscription_name', count: 10)
+        expect(request).to have_been_requested
+      end
     end
   end
 end
