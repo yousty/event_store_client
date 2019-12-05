222d8f3b1b53d4803a2b641da7c027b3b8ed0025
Sebastian Wilgosz <sebastian@useo.pl>
2019-12-05 20:04:41 +0100

    Fix bugs for deserialized_events
    Added also test coverage for the default mapper

--- lib/event_store_client/mapper/default.rb
+++ lib/event_store_client/mapper/default.rb
@@ -15,19 +15,18 @@ module EventStoreClient
         metadata = serializer.deserialize(event.metadata)
         data = serializer.deserialize(event.data)
 
-        event_class = event.type.safe_constantize
-        if event_class
-          event_class.new(
-            metadata: metadata,
-            data: data
-          )
-        else
-          EventStoreClient::DeserializedEvent.new(
-            metadata: metadata,
-            data: data,
-            type: event.type
-          )
-        end
+        event_class =
+          begin
+            Object.const_get(event.type)
+          rescue NameError
+            EventStoreClient::DeserializedEvent
+          end
+
+        event_class.new(
+          metadata: metadata,
+          data: data,
+          type: event.type
+        )
       end
 
       private
