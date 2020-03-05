cc5fa03ef8010dfde10b42ebde4c38487e03ed08
Konrad Badzioch <konrad.badzioch@useo.pl>
2020-03-05 13:23:02 +0100

    Fix Event class initialize method PRO-2646

--- lib/event_store_client/event.rb
+++ lib/event_store_client/event.rb
@@ -6,8 +6,7 @@ require 'json'
 
 module EventStoreClient
   class Event < Dry::Struct
-    attr_reader :id
-
+    attribute :id, Types::Strict::String.optional.default(nil)
     attribute :type, Types::Strict::String
     attribute :title, Types::Strict::String.optional.default(nil)
     attribute :data, Types::Strict::String.default('{}')
@@ -16,7 +15,7 @@ module EventStoreClient
     private
 
     def initialize(**args)
-      @id = SecureRandom.uuid
+      id = args[:id] || SecureRandom.uuid
       hash_meta = JSON.parse(args[:metadata] || '{}')
       hash_meta['created_at'] ||= Time.now
       args[:metadata] = JSON.generate(hash_meta)
