0da8e977bfdab26c43680e9e86f5e3de3139e8d1
Sebastian Wilgosz <sebastian@useo.pl>
2020-03-05 22:52:13 +0100

    Add documentation for encrypted mapper

--- lib/event_store_client/mapper/encrypted.rb
+++ lib/event_store_client/mapper/encrypted.rb
@@ -5,8 +5,20 @@ require 'event_store_client/data_encryptor'
 
 module EventStoreClient
   module Mapper
+    ##
+    # Transforms given event's data and encrypts/decrypts selected subset of data
+    # based on encryption schema stored in the event itself.
+
     class Encrypted
       MissingEncryptionKey = Class.new(StandardError)
+
+      ##
+      # Encrypts the given event's subset of data.
+      # Accepts specific event class instance with:
+      # * +#data+ - hash with non-encrypted values.
+      # * encryption_schema - hash with information which data to encrypt and
+      #   which key should be used as an identifier.
+      # *Returns*: General +Event+ instance with encrypted data
 
       def serialize(event)
         encryption_schema = (
@@ -28,12 +40,24 @@ module EventStoreClient
         )
       end
 
+      ##
+      # Decrypts the given event's subset of data.
+      # General +Event+ instance with encrypted data
+      # * +#data+ - hash with encrypted values.
+      # * encryption_metadata - hash with information which data to encrypt and
+      #   which key should be used as an identifier.
+      # *Returns*: Specific event class with all data decrypted
+
       def deserialize(_event); end
 
       private
 
       attr_reader :key_repository, :serializer
 
+      ##
+      # Initializes the mapper with required dependencies. Accepts:
+      # * +key_repoistory+ - repository stored encryption keys. Passed down to the +DataEncryptor+
+      # * +serializer+ - object used to serialize data. By default JSON serializer is used.
       def initialize(key_repository, serializer: Serializer::Json)
         @key_repository         = key_repository
         @serializer             = serializer
