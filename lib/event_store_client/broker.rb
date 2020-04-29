89f68e54ae92a65ea8a82efbde5c1607cdae9e56
Konrad Badzioch <konrad.badzioch@useo.pl>
2020-04-29 19:56:13 +0200

    Fix Broker#call method

--- lib/event_store_client/broker.rb
+++ lib/event_store_client/broker.rb
@@ -4,7 +4,7 @@ module EventStoreClient
   class Broker
     def call(subscriptions)
       subscriptions.each do |subscription|
-        new_events = connection.consume_feed(subscription.stream, subscription.name)
+        new_events = connection.consume_feed(subscription.stream, subscription.name) || []
         next if new_events.none?
         new_events.each { |event| subscription.subscriber.call(event) }
       end
