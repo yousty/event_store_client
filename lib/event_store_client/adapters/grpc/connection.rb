00309bd34f991f71bca6cfd8517947caa37e4af7
Sebastian Wilgosz <sebastian@useo.pl>
2021-01-13 23:34:45 +0100

    Add basic authorization for connection

--- lib/event_store_client/adapters/grpc/connection.rb
+++ lib/event_store_client/adapters/grpc/connection.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'grpc'
+require "base64"
 
 module EventStoreClient
   module GRPC
@@ -11,9 +12,15 @@ module EventStoreClient
       # to create working gRPC connection - Refer to generated grpc files
       # @return [Stub] Instance of a given `Stub` klass
       #
-      def call(stub_klass)
+      def call(stub_klass, options: {})
+        credentials =
+          options[:credentials] ||
+            Base64.encode64("#{config.eventstore_user}:#{config.eventstore_password}")
+
         stub_klass.new(
-          config.eventstore_url.to_s, :this_channel_is_insecure
+          config.eventstore_url.to_s,
+          :this_channel_is_insecure,
+          channel_args: { 'authorization' => "Basic #{credentials}"   }
         )
       end
     end
