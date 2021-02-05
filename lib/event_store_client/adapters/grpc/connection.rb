4895bcf03866fd53ceab41208809ded08886ba33
Sebastian Wilgosz <sebastian@useo.pl>
2021-02-05 03:17:19 +0100

    Add ssl support for the grpc adapter

--- lib/event_store_client/adapters/grpc/connection.rb
+++ lib/event_store_client/adapters/grpc/connection.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require 'grpc'
-require "base64"
+require 'base64'
+require 'net/http'
 
 module EventStoreClient
   module GRPC
@@ -15,12 +16,35 @@ module EventStoreClient
       def call(stub_klass, options: {})
         credentials =
           options[:credentials] ||
-            Base64.encode64("#{config.eventstore_user}:#{config.eventstore_password}")
+          Base64.encode64("#{config.eventstore_user}:#{config.eventstore_password}")
 
-        service = EventStore::Client::Streams::Streams::Stub.new(
+        stub_klass.new(
           config.eventstore_url.to_s,
-          :this_channel_is_insecure
+          channel_credentials,
+          channel_args: { 'authorization' => "Basic #{credentials.delete("\n")}" }
         )
+      end
+
+      private
+
+      attr_reader :cert
+
+      def initialize
+        @cert =
+          Net::HTTP.start(
+            config.eventstore_url.host, config.eventstore_url.port,
+            use_ssl: true,
+            verify_mode: verify_ssl,
+            &:peer_cert
+          )
+      end
+
+      def channel_credentials
+        GRPC::Core::ChannelCredentials.new(cert.to_s)
+      end
+
+      def verify_ssl
+        config.verify_ssl || OpenSSL::SSL::VERIFY_NONE
       end
     end
   end
